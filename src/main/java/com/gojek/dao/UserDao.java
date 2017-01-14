bd33b801e0329896cb2db79222096dd18cdb84d6
sandipanbiswasonedirect <sandipan.biswas@onedirect.in>
2017-01-14 13:41:45 +0530

    dao layer

---
+++ src/main/java/com/gojek/dao/UserDao.java
@@ -0,0 +1,19 @@
+package com.gojek.dao;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.gojek.entity.User;
+
+@Transactional
+public interface UserDao extends CrudRepository<User, Long> {
+
+  /**
+   * This method will find an User instance in the database by its email.
+   * Note that this method is not implemented and its working code will be
+   * automagically generated from its signature by Spring Data JPA.
+   */
+  public User findByEmail(String email);
+
+}
